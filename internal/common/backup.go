bb4e191d277e2a7b88a12caddd01ac748889d8f2
Juan Manuel Parrilla Madrid <jparrill@redhat.com>
2024-04-03 12:56:10 +0200

    Checking resources and creating secrets pre-backup in the MGMT cluster
    Signed-off-by: Juan Manuel Parrilla Madrid <jparrill@redhat.com>

--- internal/common/backup.go
+++ internal/common/backup.go
@@ -1,11 +1,19 @@
 package common
 
 import (
+	"context"
+	"fmt"
+
+	hyperv1 "github.com/openshift/hypershift/api/hypershift/v1beta1"
 	"github.com/sirupsen/logrus"
 	v1 "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // BackupPlugin is a backup item action plugin for Hypershift common objects.
@@ -54,6 +62,7 @@ func (p *BackupPlugin) AppliesTo() (velero.ResourceSelector, error) {
 // in this case, setting a custom annotation on the item being backed up.
 func (p *BackupPlugin) Execute(item runtime.Unstructured, backup *v1.Backup) (runtime.Unstructured, []velero.ResourceIdentifier, error) {
 	p.log.Info("[common-backup] Entering Hypershift common backup plugin")
+	ctx := context.Context(context.TODO())
 
 	metadata, err := meta.Accessor(item)
 	if err != nil {
@@ -69,6 +78,36 @@ func (p *BackupPlugin) Execute(item runtime.Unstructured, backup *v1.Backup) (ru
 		p.log.Infof("[common-backup] Setting annotation for item, %s", metadata.GetName())
 		annotations[CommonBackupAnnotationName] = string(BackupStatusInProgress)
 		metadata.SetAnnotations(annotations)
+	}
+
+	p.log.Infof("[common-backup] Checking NodePool resources", metadata.GetName())
+	client, err := GetClient()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nps := &hyperv1.NodePoolList{}
+	if err := client.List(ctx, nps, crclient.InNamespace("clusters")); err != nil {
+		return nil, nil, err
+	}
+
+	p.log.Info("[common-backup] Creating Test Secret per existent NodePool")
+
+	for _, np := range nps.Items {
+		p.log.Infof("[common-backup] Creating test Secret per existent NodePool %s", np.Name)
+		if err := client.Create(ctx, crclient.Object(&corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      fmt.Sprintf("test-secret-%s", np.Name),
+				Namespace: "clusters",
+			},
+			Data: map[string][]byte{
+				"test": []byte("test"),
+			},
+		})); err != nil {
+			if !apierrors.IsAlreadyExists(err) {
+				return nil, nil, err
+			}
+		}
 	}
 
 	return item, nil, nil
--- internal/common/utils.go
+++ internal/common/utils.go
@@ -1,12 +1,14 @@
 package common
 
 import (
-	"github.com/pkg/errors"
+	"fmt"
+
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
+	cr "sigs.k8s.io/controller-runtime"
+	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func getMetadataAndAnnotations(item runtime.Unstructured) (metav1.Object, map[string]string, error) {
@@ -23,19 +25,42 @@ func getMetadataAndAnnotations(item runtime.Unstructured) (metav1.Object, map[st
 	return metadata, annotations, nil
 }
 
-func GetClient() (*kubernetes.Clientset, error) {
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	configOverrides := &clientcmd.ConfigOverrides{}
-	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
-	clientConfig, err := kubeConfig.ClientConfig()
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
+//func GetClient() (*kubernetes.Clientset, error) {
+//	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+//	configOverrides := &clientcmd.ConfigOverrides{}
+//	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+//	clientConfig, err := kubeConfig.ClientConfig()
+//	if err != nil {
+//		return nil, errors.WithStack(err)
+//	}
+//
+//	client, err := kubernetes.NewForConfig(clientConfig)
+//	if err != nil {
+//		return nil, errors.WithStack(err)
+//	}
+//
+//	return client, nil
+//}
 
-	client, err := kubernetes.NewForConfig(clientConfig)
+// GetClient creates a controller-runtime client for Kubernetes
+func GetClient() (crclient.Client, error) {
+	config, err := GetConfig()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, fmt.Errorf("unable to get kubernetes config: %w", err)
 	}
-
+	client, err := crclient.New(config, crclient.Options{Scheme: CustomScheme})
+	if err != nil {
+		return nil, fmt.Errorf("unable to get kubernetes client: %w", err)
+	}
 	return client, nil
+}
+
+func GetConfig() (*rest.Config, error) {
+	cfg, err := cr.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+	cfg.QPS = 100
+	cfg.Burst = 100
+	return cfg, nil
 }
